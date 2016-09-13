0bf3988f14ba6c344387ce07d0540d73a8d6077e
coreentin <core.entin@gmail.com>
2016-09-14 01:37:13 +0200

    test from cyanogen nvidia devices

---
+++ BoardConfig.mk
@@ -0,0 +1,149 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+# inherit from the proprietary version
+-include vendor/nvidia/s8515/BoardConfigVendor.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/nvidia/s8515/include
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := $(TARGET_CPU_VARIANT)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := s8515
+
+# Board
+TARGET_BOARD_PLATFORM := tegra
+TARGET_TEGRA_VERSION := t114
+TARGET_TEGRA_FAMILY := t11x
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_USE_TEGRA_BIONIC_OPTIMIZATION := true
+TARGET_USE_TEGRA11_MEMCPY_OPTIMIZATION := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
+
+# Optimization Flags
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mcpu=cortex-a9 -mfpu=neon
+TARGET_GLOBAL_CFLAGS += -fpic -fno-short-enums -pipe -funsafe-math-optimizations -ffinite-math-only
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mcpu=cortex-a9 -mfpu=neon
+TARGET_GLOBAL_CPPFLAGS += -fpic -fno-short-enums -pipe -funsafe-math-optimizations -ffinite-math-only
+TARGET_EXTRA_CFLAGS += $(call cc-option,  -marm -march=armv7-a)
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := true
+# Remove if/when M libs are released
+#TARGET_TINY_ALSA_IGNORE_SILENCE_SIZE := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/nvidia/s8515
+TARGET_KERNEL_CONFIG := tegra_s8515_cyanogenmod_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.hardware=s8515
+#BOARD_KERNEL_CMDLINE := "androidboot.selinux=permissive"
+NEED_WORKAROUND_CORTEX_A9_745320 := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/nvidia/s8515/bluetooth
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+
+#TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
+TARGET_USES_GL_VENDOR_EXTENSIONS := true
+#BOARD_EGL_WORKAROUND_BUG_10194508 := true
+#TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+#BOARD_USE_MHEAP_SCREENSHOT := true
+#BOARD_EGL_SKIP_FIRST_DEQUEUE := true
+
+# Partition
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 9085824
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 805306368
+#BOARD_USERDATAIMAGE_PARTITION_SIZE  := 2598371328
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2597674112
+BOARD_FLASH_BLOCK_SIZE := 4096
+#BOARD_KERNEL_BASE := 0x10000000
+#BOARD_KERNEL_PAGESIZE := 2048
+#BOARD_HAS_LARGE_FILESYSTEM := true
+
+#SET_DCP_CURRENT_LIMIT_2A := true
+#USE_E2FSPROGS := true
+
+COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DMR0_CAMERA_BLOB -DNEEDS_VECTORIMPL_SYMBOLS -D__ARM_CACHE_LINE_SIZE=32
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := tegra
+
+# Recovery
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_RECOVERY_FSTAB := device/nvidia/s8515/rootdir/etc/fstab.ceres
+#TARGET_RECOVERY_DEVICE_DIRS += device/nvidia/s8515
+#TW_BOARD_CUSTOM_GRAPHICS := ../../../device/nvidia/s8515/recovery/graphics.c
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/tegra-udc.0/gadget/lun0/file
+TW_BRIGHTNESS_PATH := /sys/devices/platform/pwm-backlight/backlight/pwm-backlight/brightness
+TW_MAX_BRIGHTESS := 255
+TW_THEME := portrait_hdpi
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_STA          := "/data/misc/wifi/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/data/misc/wifi/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+
+BOARD_HARDWARE_CLASS := device/nvidia/s8515/cmhw/
+
+# Fix mkbootimg problem when compiling
+LOCAL_PACK_MODULE_RELOCATIONS := false
+
+
+BOARD_SEPOLICY_DIRS := device/nvidia/s8515/sepolicy/common \
+                       device/nvidia/s8515/sepolicy/raydium \
+                       device/nvidia/s8515/sepolicy/product
+
+# Vendor Init
+TARGET_INIT_VENDOR_LIB := libinit_s8515
+TARGET_LIBINIT_DEFINES_FILE := device/nvidia/s8515/init/init_s8515.c
---
+++ README.md
@@ -0,0 +1,41 @@
+# Device configuration for NVIDIA S8515 
+(Wiko WAX 4G, Blackphone 1 and explay 4game)
+
+## Spec Sheet
+| Feature                 | Specification                     |
+| :---------------------- | :-------------------------------- |
+| CPU                     | Quad Core 1.7GHz                  |
+| Chipset                 | NVIDIA® Tegra 4i SL440            |
+| GPU                     | Nvidia GeForce ULP                |
+| Memory                  | 1GB RAM                           |
+| Shipped Android Version | 4.3                               |
+| Storage                 | 4GB                               |
+| MicroSD                 | Up to 32GB                        |
+| Battery                 | 2000 mAh                          |
+| Dimentions              | 140.1 x 67.5 x 8.8 mm             |
+| Display                 | 1280 x 720 pixels                 |
+| Release Date            | April, 2014                       |
+
+## Device Picture
+![NVIDIA WIKO WAX ](http://www.nvidia.fr/content/tegra/images/wiko-wax/large-wiko-wax-white.png "NVIDIA WIKO WAX")
+
+## Copyright
+
+```
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+```
+
